6d6184364cb69d9f954d55472a6a6f6c5e15d639
nvdinh185 <nvdinh0905671805@gmail.com>
2019-01-11 15:52:06 +0700

    first commit

--- src/components/components.module.ts
+++
@@ -1,8 +0,0 @@
-import { NgModule } from '@angular/core';
-import { ImageComponent } from './image/image';
-@NgModule({
-	declarations: [ImageComponent],
-	imports: [],
-	exports: [ImageComponent]
-})
-export class ComponentsModule {}
