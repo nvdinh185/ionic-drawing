b579f0828a33d6ce324f31353c7c3bb89f338fc1
nvdinh185 <nvdinh0905671805@gmail.com>
2019-01-11 08:48:47 +0700

    first commit

---
+++ src/pages/home/home.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import {} from 'ionic-angular';
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+
+  constructor() {}
+}
